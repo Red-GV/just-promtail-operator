64530ed32c2de9633724d243ca88ae288b57bbe3
Hui Kang <hkang@redhat.com>
2021-02-04 16:04:34 +0000

    init commit

---
+++ .bingo/Variables.mk
@@ -0,0 +1,25 @@
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.3.1. DO NOT EDIT.
+# All tools are designed to be build inside $GOBIN.
+BINGO_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+GOPATH ?= $(shell go env GOPATH)
+GOBIN  ?= $(firstword $(subst :, ,${GOPATH}))/bin
+GO     ?= $(shell which go)
+
+# Bellow generated variables ensure that every time a tool under each variable is invoked, the correct version
+# will be used; reinstalling only if needed.
+# For example for operator-sdk variable:
+#
+# In your main Makefile (for non array binaries):
+#
+#include .bingo/Variables.mk # Assuming -dir was set to .bingo .
+#
+#command: $(OPERATOR_SDK)
+#	@echo "Running operator-sdk"
+#	@$(OPERATOR_SDK) <flags/args..>
+#
+OPERATOR_SDK := $(GOBIN)/operator-sdk-v1.3.0
+$(OPERATOR_SDK): $(BINGO_DIR)/operator-sdk.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/operator-sdk-v1.3.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=operator-sdk.mod -o=$(GOBIN)/operator-sdk-v1.3.0 "github.com/operator-framework/operator-sdk/cmd/operator-sdk"
+
